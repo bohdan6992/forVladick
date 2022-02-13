0950c9eddf9ef5aff6072c633ac13896ff5611b1
KirpaOleg <profix.ok@gmail.com>
2022-02-13 14:48:31 +0200

    feature/dbuser3

--- model/users.js
+++ model/users.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const createUser = async (obj) => {
   const user = new User({
-    name: obj.name,
+    username: obj.name,
     nickname: obj.nickname,
     email: obj.email,
     password: obj.password, 
