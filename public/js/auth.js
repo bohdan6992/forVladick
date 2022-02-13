bb96146a205b437402ec44fa876d8f97a9183f2d
bohdan6992 <bohdanmaljar@gmail.com>
2022-02-13 21:26:42 +0200

    before pullrequest

---
+++ public/js/auth.js
@@ -0,0 +1,29 @@
+const formEl = document.querySelector('form');
+const answEl = document.querySelector('.answer');
+const startBtn = document.querySelector('.button-click');
+
+formEl.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const data = new FormData(formEl);
+  let url = window.location.pathname.split('/');
+  if (url[2] === 'registration') {
+    url = '/auth/registration' 
+  } 
+  axios
+    .post(url, data)
+    .then((r) => {
+      if (!r.data.page) {
+        window.Headers
+        answEl.innerHTML = r.data;
+      } else {
+        window.location.replace(`http://127.0.0.1:3000${r.data}`);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+startBtn.addEventListener('click', () => {
+  answEl.style.display = 'block';
+});
--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,33 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const upload = multer();
-const { createUser } = require('../model/users');
+const { check } = require('express-validator');
 const authController = require('../controller/auth');
 
+router.get('/registration', authController.getRegistrationPage);
 
-router.get('/', async (req, res) => {
-  const user = await createUser();
-  res.send('user is ok');
-});
+router.post('/registration', upload.none(), 
+[
+  check('username')
+    .notEmpty()
+    .withMessage('Username can\'t be empty'),
+
+  check('nickname')
+    .notEmpty()
+    .withMessage('Nickname can\'t be empty'),
+
+  check('email')
+    .notEmpty()
+    .withMessage('Email can\'t be empty')
+    .isEmail()
+    .withMessage('Email isn\'t valid'),
+
+  check('password')
+    .notEmpty()
+    .withMessage('Pasword can\'t be empty')
+    .isLength({min:8})
+    .withMessage('Password must be more than 8 characters'),
+], 
+authController.registration);
 
 module.exports = router;
