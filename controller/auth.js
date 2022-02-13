a93021745612fee176e8aa2c7c34754ce67bacae
KirpaOleg <profix.ok@gmail.com>
2022-02-13 16:12:34 +0200

    feture/roles

---
+++ controller/auth.js
@@ -0,0 +1,10 @@
+const Role = require('../model/schemas/role');
+
+const authController = {
+
+  login: async (req, res) => {
+
+  }
+};
+
+module.exports = authController;
--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const multer = require('multer');
 const upload = multer();
 const { createUser } = require('../model/users');
+const authController = require('../controller/auth');
 
 
 router.get('/', async (req, res) => {
-  const user = await createUser(data);
+  const user = await createUser();
   res.send('user is ok');
 });
 
